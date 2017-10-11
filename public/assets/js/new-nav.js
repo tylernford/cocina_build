ed1fac21912bb7a9b6253b9e8b5e473c7894157b
Tyler N. Ford <tylernford@gmail.com>
2017-10-10 20:34:18 -0500

    Deleted local 'Public' folder

--- public/assets/js/new-nav.js
+++
@@ -1,6 +0,0 @@
-(function() {
-    function transformStart() {
-        document.querySelector(".js-nav").classList.toggle("is-visible");
-    }
-    document.querySelector("#js-menu").addEventListener("click", transformStart);
-})();
