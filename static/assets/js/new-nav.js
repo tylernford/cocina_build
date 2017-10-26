2a1af25de12ff384b3402057b0d9617793b1a648
Tyler N. Ford <tylernford@gmail.com>
2017-10-26 08:11:15 -0500

    Eh

--- static/assets/js/new-nav.js
+++ static/assets/js/new-nav.js
@@ -1,7 +1,6 @@
 (function() {
     function transformStart() {
         document.querySelector(".js-nav").classList.toggle("is-visible");
-        document.querySelector("body").classList.toggle("overflow-hidden");
     }
     document.querySelector("#js-menu").addEventListener("click", transformStart);
 })();
